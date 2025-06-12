92dbc98c9ba9a64583539bd3fe2c7693bb765980
NathanDalling <Nathandall@aol.com>
2025-06-12 11:23:06 -0400

    create no erroes

--- main.tf
+++ main.tf
@@ -50,10 +50,9 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name = "blog_new"
-
+  
+  name = "blog"
   vpc_id = module.blog_vpc.vpc_id
-
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
